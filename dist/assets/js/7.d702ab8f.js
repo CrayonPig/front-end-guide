06297cd1e22382f3ffe63fb701e9277e9c63942b
CrayonPig <475003925@qq.com>
2023-01-10 14:51:53 +0800

    feat: 激活dist路径

---
+++ dist/assets/js/7.d702ab8f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{278:function(t,s,n){"use strict";n.r(s);var e=n(13),o=Object(e.a)({},(function(){var t=this._self._c;return t("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[t("p",[this._v("fsfd")])])}),[],!1,null,null,null);s.default=o.exports}}]);
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: '前端开发规范',
-  description: '欢迎使用前端代码规范，使用过程中如碰到问题，请到Github进行提问。 https://github.com/CrayonPig',
+  description: '欢迎使用前端代码规范，使用过程中如碰到问题，请到Github进行提问。 https://github.com/CrayonPig/front-end-guide',
   dest: './dist/',
   markdown: {
     lineNumbers: true
