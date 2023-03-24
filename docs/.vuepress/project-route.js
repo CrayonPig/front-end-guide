36ffe8374927ab68fc4f27ed175c8e89562217c7
CrayonPig <475003925@qq.com>
2023-03-24 18:24:48 +0800

    feat: 添加索引

--- docs/.vuepress/project-route.js
+++ docs/.vuepress/project-route.js
@@ -18,8 +18,23 @@ module.exports = {
         '/project/develop/module',
         '/project/develop/guide',
         '/project/develop/lint',
-        '/project/develop/build',
         '/project/develop/debug',
+        '/project/develop/cli',
+      ]
+    },
+    {
+      title: '构建',
+      collapsable: false, // 可选的, 默认值是 true,
+      children: [
+        '/project/build/build',
+        '/project/build/ast',
+      ]
+    },
+    {
+      title: '测试',
+      collapsable: false, // 可选的, 默认值是 true,
+      children: [
+        '/project/test/unit',
       ]
     },
   ],
